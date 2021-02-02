6109b94f0da64cb477a77ac0a694880ff561ee69
Salma Abigail Lopez Dominguez <salma@MBP-de-Salma.lan>
2021-02-02 17:14:46 -0600

    Task Tracker App

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "./Button";
+import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green "}
+          text={showAdd ? "close" : "add"}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+/*const headingStyle = {
+  color: "red",
+  backgroundColor: "black",
+};*/
+
+export default Header;
